09dbb05d1150907a446ebd5ad8a5cf2a8785c711
rs2518 <rs2518@ic.ac.uk>
2020-07-02 12:47:55 +0100

    Add game board as numpy array

---
+++ connect_4.py
@@ -0,0 +1,17 @@
+#import sys
+import numpy as np
+#import pygame
+
+
+# Create static variables
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+# Create board as a numpy array
+def create_board():
+    return np.zeros((ROW_COUNT, COLUMN_COUNT))
+
+
+
+#board = create_board()
+#print(board)
