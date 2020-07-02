d24c386e09ddcfd9beb39cedfa9defa86c03329b
rs2518 <rs2518@ic.ac.uk>
2020-07-02 12:55:23 +0100

    Initialise game loop

--- connect_4.py
+++ connect_4.py
@@ -12,6 +12,28 @@ def create_board():
     return np.zeros((ROW_COUNT, COLUMN_COUNT))
 
 
+# Code for gameplay
+game_over = False
+board = create_board()
+turn = 0
 
+while not game_over:
+    # Player 1's turn
+    if turn == 0:
+        col = input("It's Player 1's turn! Select a number (0-6): ")
+        
+        # Add piece if turn is valid
+
+    # Player 2's turn
+    else:
+        col = input("It's Player 2's turn! Select a number (0-6): ")
+        
+        # Add piece if turn is valid
+    
+    # Alternate to next players turn
+    turn += 1
+    turn = turn % 2
+    
+    
 #board = create_board()
 #print(board)
