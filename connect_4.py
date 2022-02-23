81641fc049ae5ac7859d6db8b8015eaf0d79d8c2
rs2518 <rs2518@ic.ac.uk>
2022-02-23 16:02:51 +0000

    Add game graphics

--- connect_4.py
+++ connect_4.py
@@ -1,6 +1,6 @@
-#import sys
+import sys
 import numpy as np
-import pygame     # conda install is FAULTY. Use pip
+import pygame as pg     # conda install is FAULTY. Use pip
 
 
 # Create static variables
@@ -8,6 +8,26 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 N_CONNECTS = 4
 
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+PIECES = {1:RED, -1:YELLOW, 0:WHITE}    # Piece:Colour (including empty slot)
+SQUARE_SIZE = 100
+RADIUS = int(SQUARE_SIZE/2 - 5)
+OFFSET = int(SQUARE_SIZE/2)
+
+width = COLUMN_COUNT * SQUARE_SIZE
+height = (ROW_COUNT + 1) * SQUARE_SIZE
+size = (width, height)
+SCREEN_RECT = ((0, 0), size)
+BOARD_RECT = ((0, SQUARE_SIZE), (width, height - SQUARE_SIZE))
+
+
+
+# =============================================================================
+# UTILITIES
+# =============================================================================
 # Create board as a numpy array
 def create_board():
     return np.zeros((ROW_COUNT, COLUMN_COUNT))
@@ -64,8 +84,37 @@ def is_winning_move(board, piece):
             kernel = board[row:row+N_CONNECTS, col:col+N_CONNECTS]
             if check_wins(kernel, piece):
                 return True
+
+
+# Function for player's move
+def player_move(board, col, turn):
+    piece = list(PIECES.keys())[turn]
+    if is_valid_location(board, col):
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, piece)
+        
+        if is_winning_move(board, piece):
+            return True
+        else:
+            return False
             
+# Draw board with pygame graphics
+def draw_board(board):
+    # Draw background and board
+    pg.draw.rect(screen, WHITE, ((0, 0), size))
+    pg.draw.rect(screen, BLUE,
+                 ((0, SQUARE_SIZE), (width, height-SQUARE_SIZE)))
     
+    # Draw pieces and re-render screen
+    for col in range(COLUMN_COUNT):
+        for row in range(ROW_COUNT):
+            slot = board[row][col]
+            pg.draw.circle(
+                screen, PIECES[slot],
+                (SQUARE_SIZE*col+OFFSET, height-(SQUARE_SIZE*row+OFFSET)),
+                RADIUS)
+    pg.display.update()
+                    
 
 # Code for gameplay. Initialise variables for loop
 game_over = False
@@ -73,46 +122,47 @@ board = create_board()
 print_board(board)
 turn = 0
 
+
+
+# =============================================================================
+# GAMEPLAY
+# =============================================================================
+# Initialise pygame
+pg.init()
+screen = pg.display.set_mode(size)
+draw_board(board)
+pg.display.update()
+
 while not game_over:
-    # Player 1's turn
-    if turn == 0:
-        piece = 1
-        col = int(input("It's Player 1's turn! Select a number (0-6): "))
+    for event in pg.event.get():
+        # Exit system when game ends
+        if event.type == pg.QUIT:
+            sys.exit()
         
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, piece)
+        # Render animation for piece along top of screen
+        if event.type == pg.MOUSEMOTION:
+            # Stop previously rendered pieces remaining on the screen
+            pg.draw.rect(screen, WHITE, ((0, 0), (width, SQUARE_SIZE)))
             
-            if is_winning_move(board, piece):
-                print("Player 1 wins!!!")
-                game_over = True
-
-
-    # Player 2's turn
-    else:
-        piece = -1
-        col = int(input("It's Player 2's turn! Select a number (0-6): "))
+            piece = PIECES[list(PIECES.keys())[turn]]   # Player colour
+            pg.draw.circle(screen, piece, (event.pos[0], OFFSET), RADIUS)
+        pg.display.update()
         
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, piece)
+        # Set event for dropping a piece (mouse click)
+        if event.type == pg.MOUSEBUTTONDOWN:
+            col = int(event.pos[0]//SQUARE_SIZE)
+            game_over = player_move(board, col, turn)
+        
+            print_board(board)
+            print("")
+            if game_over:
+                print("Player ", turn+1, " wins!!!")
+            draw_board(board)
             
-            if is_winning_move(board, piece):
-                print("Player 2 wins!!!")
-                game_over = True
-                
-    
-    print_board(board)
-    
-    # Alternate to next players turn
-    turn += 1
-    turn = turn % 2
-
-    
-   
-#board = create_board()
-#print(board)
-
-
-
-# TIMESTAMP: Video 2 @10:00
+            # Alternate to next players turn
+            turn += 1
+            turn = turn % 2
+            
+            # Wait 3 seconds before closing game
+            if game_over:
+                pg.time.wait(3000)
